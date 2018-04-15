8ffe58cc83f44948fd095591b5108943b23694cb
yangroro <529yhs@gmail.com>
2018-04-15 17:46:54 +0900

    Add docker-compose example project

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:9.11
+
+ENV REDIS_URL='redis://heechan.local'
+WORKDIR app
+COPY ./package.json package.json
+COPY ./yarn.lock yarn.lock
+RUN yarn install
+COPY ./index.js index.js
+EXPOSE 8080
+ENTRYPOINT ["node"]
+CMD [ "index.js" ]
---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('start server');
+var http = require("http");
+var redis = require("redis");
+var redisUrl = process.env.REDIS_URL;
+console.log('start to connect redis: ' + redisUrl);
+var client = redis.createClient({
+    url: redisUrl
+});
+console.log('redis was connected');
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    client.incr(req.url, function (error, value) {
+        var viewCount = value;
+        var response = { 'viewCount': viewCount };
+        res.write(JSON.stringify(response));
+        res.end();
+    });
+}).listen(8080);
